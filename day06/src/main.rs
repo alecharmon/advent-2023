800167682c10364beded75df9cea9922592c2706
Alec Harmon <alec@onesignal.com>
2023-12-06 17:51:14 -0500

    Day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,23 +1,43 @@
 use regex::Regex;
 
-type InputType = Vec<(u32, u32)>;
+type InputType = Vec<(u64, u64)>;
 
-fn part1(input: InputType) -> u32 {
-    let mut sum = 0;
-
-    sum
+fn part1(input: InputType) -> u64 {
+    let res: Vec<u64> = input
+        .iter()
+        .map(|(time, distance)| {
+            let mut count: u64 = 0;
+            for amount_held in 0..time.to_owned() {
+                if (amount_held * (time.to_owned() - amount_held)) > *distance {
+                    count += 1;
+                }
+            }
+            count
+        })
+        .collect();
+    res.iter().map(|f| f.to_owned()).product()
 }
 
-fn part2(input: InputType) -> usize {
-    0
+fn part2(input: InputType) -> u64 {
+    // truly i ended up just deleting the white space but figured that was dishonest
+    let mut time_str = String::new();
+    let mut distance_str = String::new();
+    input.iter().for_each(|(time, distance)| {
+        time_str += time.to_string().as_str();
+        distance_str += distance.to_string().as_str();
+    });
+
+    part1(vec![(
+        time_str.parse().unwrap(),
+        distance_str.parse().unwrap(),
+    )])
 }
 
 fn parse() -> InputType {
     let numbers_pattern = Regex::new(r"(\d+)").unwrap();
-    let lines: Vec<Vec<u32>> = include_str!("../input.txt")
+    let lines: Vec<Vec<u64>> = include_str!("../input.txt")
         .lines()
         .map(|line| {
-            let rhs: &str = line.split(':').collect::<Vec<&str>>()[1];
             numbers_pattern
                 .captures_iter(line)
                 .map(|m| m.get(1).unwrap().as_str().parse().unwrap())
@@ -33,9 +53,7 @@ fn parse() -> InputType {
 
 fn main() {
     let r1 = part1(parse());
-    /// The line `// let r2 = part2(parse());` is commented out, which means it is not being executed. It is a placeholder
-    /// for calling the `part2` function with the parsed input and storing the result in the variable `r2`. Currently, it is
-    // let r2 = part2(parse());
-    // println!("Part 1: {r1}");
-    // println!("Part 2: {r2}");
+    let r2 = part2(parse());
+    println!("Part 1: {r1}");
+    println!("Part 2: {r2}");
 }
